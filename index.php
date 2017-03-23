549172cfdad38dbc9de051c9d335088034ae2f77
Marcos Vasconcelos <Marcos Vasconcelos@10.224.160.227>
2017-03-23 13:06:00 -0300

    TESTANDO DO NOTEBOOK!

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-       echo 'Projeto casa limpa';
+       echo 'Projeto casa limpa commitando do notebook!';
         ?>
     </body>
 </html>
