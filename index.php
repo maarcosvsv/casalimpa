03f716d0bfd2a59c74a83a4a1759dcc9883e2936
maarcosvsv <maarcosvsv@gmail.com>
2017-03-23 13:06:27 -0300

    Comitt teste final.

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-       echo 'epa marcossads dsasdaa';
+       echo 'Projeto casa limpa';
         ?>
     </body>
 </html>
