87d4a5043b8c29a2611a0d0ea9ff96ebcce60a3e
maarcosvsv <maarcosvsv@gmail.com>
2017-03-23 13:29:12 -0300

    Comittando localhost

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-       echo 'Projeto casa limpa commitando do notebook!';
+       echo 'Marquin';
         ?>
     </body>
 </html>
