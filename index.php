d3f09acb40033e7175413cc9d9d04525a1d7c106
danie <danie@NOT-SANTS>
2017-04-08 14:53:39 -0300

    TESTANDO NETBEANS NOTEBOOK DANIEL

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-       echo 'Projeto de pi';
+       echo 'Projeto de pi DANIEL TESTE NETBEANS';
         ?>
     </body>
 </html>
