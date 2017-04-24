668319636c18c11c82ba9e9c85acd5f800c8b0bc
fabiorfilho <fabiomendesbiz@gmail.com>
2017-04-24 11:09:05 -0300

    Link do cadastro com Bootstrap

--- pages/cadastroEmpregado.php
+++ pages/cadastroEmpregado.php
@@ -12,6 +12,18 @@
 
 <meta charset="utf-8">
 
+<!-- Bootstrap -->
+
+    <link href="resources/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="resources/css/landing-page.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="resources/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+
 </head>
 
 <body>
