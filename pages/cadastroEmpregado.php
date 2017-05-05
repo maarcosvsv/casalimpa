abc6d3d763026a1c6c22a6c00a380961a56159f9
Marcos Vasconcelos <Marcos Vasconcelos@10.224.160.16>
2017-05-05 15:34:05 -0300

    Estilo criado pelo fábio adicionado.

--- pages/cadastroEmpregado.php
+++ pages/cadastroEmpregado.php
@@ -3,32 +3,73 @@
 <head>
 <!-- DEVE SER SEGUIDO ESTA ORDEM ABAIXO! -->
    <script language="JavaScript" type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script> 
- <script language="JavaScript" type="text/javascript" src="../resources/js/jquery.js"></script>
+ 	<script language="JavaScript" type="text/javascript" src="../resources/js/jquery.js"></script>
    <script language="JavaScript" type="text/javascript" src="../resources/js/script.js"></script>
- <script language="JavaScript" type='text/javascript' src='../ajax/cep.ajax.js'></script>
+ 	<script language="JavaScript" type='text/javascript' src='../ajax/cep.ajax.js'></script>
  
- 
-<link rel="stylesheet" type="text/css" href="../resources/css/estilo.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">	
 
-<meta charset="utf-8">
+	<meta charset="utf-8">
 
-<!-- Bootstrap -->
-
-    <link href="resources/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Bootstrap Core CSS -->
+    <link href="../resources/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="resources/css/landing-page.css" rel="stylesheet">
+    <link href="../resources/css/landing-page.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="resources/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="../resources/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <link rel="stylesheet" type="text/css" href="../resources/css/estilo.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
 
 </head>
 
 <body>
-
-<div id="#">
+ <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
+        <div class="container topnav">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand topnav" href="#">imagem logo</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="#about">Sobre</a>
+                    </li>
+                    <li>
+                        <a href="#services">Como funciona</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Contato</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+<div id="form">
+	<div id="form1">
 <p> Cadastro:</P>
 <form action="../action/cadastroEmpregado.action.php" method="POST">
 	<input type="text" id="nome" name="nome" placeholder="Nome completo"/><br/>
@@ -71,7 +112,7 @@
 	
 	</form>
 	
-	
+	</div>
 
 
 </div>
